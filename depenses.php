78292d31d91ad15e60d93ef6efda74b4155798c0
Luigi <luigi.ribbi@etudiant.univ-reims.fr>
2018-03-01 16:00:18 +0100

    Structure de base de la page

--- depenses.php
+++ depenses.php
@@ -65,13 +65,43 @@ HTML
 //Ajoute le premier tag du div landing
 if($_SESSION['user']->hasColocation()){
 $p->appendContent(<<<HTML
-<div>
+<main>
+  
+  <input id="tab1" type="radio" name="tabs" checked>
+  <label class="tab" for="tab1">Journal des dépenses</label>
+    
+  <input id="tab2" type="radio" name="tabs">
+  <label class="tab" for="tab2">Créer une nouvelle dépense</label>
+    
+  <input id="tab3" type="radio" name="tabs">
+  <label class="tab" for="tab3">Créer une facture récurrente</label>
+    
+  <section id="content1">
+    
+  </section>
+    
+  <section id="content2">
+    <p>
+      Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+    </p>
+    <p>
+      Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+    </p>
+  </section>
+    
+  <section id="content3">
+    <p>
+      Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+    </p>
+    <p>
+      Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+    </p>
+  </section>
 HTML
 );
 
 //Fin de tag du div landing
 $p->appendContent(<<<HTML
-</div>
 HTML
 );
 }
@@ -137,6 +167,8 @@ $p->appendJS(<<<JS
       $('#header').removeClass('header-fixed');
     }
     });
+
+    
   })
 JS
 );
