1ef359257350750c758ec9dae408dcad449dfcc3
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2018-03-12 15:46:13 +0100

    Exportation BD SQL

--- sql/colocomax.sql
+++ sql/colocomax.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
 --
--- Hôte : localhost
--- Généré le :  Dim 04 mars 2018 à 10:13
--- Version du serveur :  10.1.30-MariaDB
--- Version de PHP :  7.2.1
+-- Client :  mysql
+-- Généré le :  Lun 12 Mars 2018 à 14:45
+-- Version du serveur :  5.5.56-MariaDB
+-- Version de PHP :  5.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `colocomax`
+-- Base de données :  `ribb0001`
 --
 
 -- --------------------------------------------------------
@@ -28,17 +26,17 @@ SET time_zone = "+00:00";
 -- Structure de la table `colocations`
 --
 
-CREATE TABLE `colocations` (
+CREATE TABLE IF NOT EXISTS `colocations` (
   `colocation_id` int(11) NOT NULL,
   `colocation_nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `adresse` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ville` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `colocation_pass` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `colocation_creator` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `colocations`
+-- Contenu de la table `colocations`
 --
 
 INSERT INTO `colocations` (`colocation_id`, `colocation_nom`, `adresse`, `ville`, `colocation_pass`, `colocation_creator`) VALUES
@@ -51,7 +49,7 @@ INSERT INTO `colocations` (`colocation_id`, `colocation_nom`, `adresse`, `ville`
 -- Structure de la table `factures`
 --
 
-CREATE TABLE `factures` (
+CREATE TABLE IF NOT EXISTS `factures` (
   `facture_id` int(11) NOT NULL,
   `libelle` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `montant` double NOT NULL,
@@ -64,23 +62,22 @@ CREATE TABLE `factures` (
 -- Structure de la table `paiements`
 --
 
-CREATE TABLE `paiements` (
+CREATE TABLE IF NOT EXISTS `paiements` (
   `montant` float NOT NULL,
   `raison` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `typePaiement` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `paiement_id` int(11) NOT NULL,
   `utilisateur_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `paiements`
+-- Contenu de la table `paiements`
 --
 
 INSERT INTO `paiements` (`montant`, `raison`, `typePaiement`, `paiement_id`, `utilisateur_id`) VALUES
-(400, 'Machine à laver', 'depense', 3, 6),
-(20, '', 'avance', 7, 6),
-(35, '', 'remboursement', 8, 6),
-(20, '', 'remboursement', 9, 4);
+(400, 'Machine à laver', 'depense', 12, 6),
+(50, '', 'avance', 13, 6),
+(45, '', 'remboursement', 14, 4);
 
 -- --------------------------------------------------------
 
@@ -88,7 +85,7 @@ INSERT INTO `paiements` (`montant`, `raison`, `typePaiement`, `paiement_id`, `ut
 -- Structure de la table `participer`
 --
 
-CREATE TABLE `participer` (
+CREATE TABLE IF NOT EXISTS `participer` (
   `type` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `montant` double NOT NULL,
   `paiement_id` int(11) NOT NULL,
@@ -96,17 +93,16 @@ CREATE TABLE `participer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `participer`
+-- Contenu de la table `participer`
 --
 
 INSERT INTO `participer` (`type`, `montant`, `paiement_id`, `utilisateur_id`) VALUES
-('partegale', 100, 3, 4),
-('partegale', 100, 3, 6),
-('partegale', 100, 3, 7),
-('partegale', 100, 3, 8),
-('partegale', 20, 7, 8),
-('partegale', 35, 8, 7),
-('partegale', 20, 9, 6);
+('partegale', 100, 12, 4),
+('partegale', 100, 12, 6),
+('partegale', 100, 12, 7),
+('partegale', 100, 12, 8),
+('partegale', 50, 13, 4),
+('partegale', 45, 14, 6);
 
 -- --------------------------------------------------------
 
@@ -114,7 +110,7 @@ INSERT INTO `participer` (`type`, `montant`, `paiement_id`, `utilisateur_id`) VA
 -- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE `utilisateurs` (
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `utilisateur_id` int(11) NOT NULL,
   `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prenom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -125,25 +121,26 @@ CREATE TABLE `utilisateurs` (
   `colocation_id` int(11) DEFAULT NULL,
   `avatar` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'placeholder.jpg',
   `LASTTIMESEEN` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `utilisateurs`
+-- Contenu de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`utilisateur_id`, `nom`, `prenom`, `date_de_naissance`, `sexe`, `pseudo`, `passwd`, `colocation_id`, `avatar`, `LASTTIMESEEN`) VALUES
 (1, 'Ribbi', 'Luigi', NULL, NULL, 'Luigito', '$2y$10$kCiZsf3hA9d9H2BzmYrkjuSd0sGhzlIe665mraLp5hN.W3bd32tEG', NULL, 'placeholder.jpg', NULL),
 (2, 'Dielh', 'Lorenza', NULL, NULL, 'mikaiix', '$2y$10$tKa9BwXLyFAWQz5hYUxIb.hZSxzvjVXX8RhUA0f2ATpz7kspIPW3a', NULL, 'placeholder.jpg', NULL),
 (3, 'Pichu', 'Lily', '1998-02-19', 'F', 'Lilypichu', '$2y$10$9z/bt.TF/jCGKKL.X4FtB.02kJgJKm01MdniCfcVkpaaXl4NBn4Z.', 1, '4ed78a3f31bde0f919033a242cb38e5f8dcb91414f1c9f0bf0c58ca3b7b10eb8.jpeg', '2018-03-03 14:16:16'),
-(4, 'Myster', 'Federico', NULL, 'M', 'Fedmyster', '$2y$10$mFuLX1IeyJL2eDJRiSqPzuDV0R/j4tTkRMTQeeVbVUbe5s/33ccOu', 1, '9883e2142f1d1bade0cbf9fa10c6f96ebd461e366df0c7c929e2d804accda74d.jpeg', '2018-03-03 17:06:41'),
+(4, 'Myster', 'Federico', NULL, 'M', 'Fedmyster', '$2y$10$mFuLX1IeyJL2eDJRiSqPzuDV0R/j4tTkRMTQeeVbVUbe5s/33ccOu', 1, '9883e2142f1d1bade0cbf9fa10c6f96ebd461e366df0c7c929e2d804accda74d.jpeg', '2018-03-12 14:43:31'),
 (5, 'Marchand', 'Antoine', NULL, NULL, 'marchand', '$2y$10$6Kbov5YtI50MlS1qaFMzR.5Gh8.badAcUmqNU9YOKkA3AgbVoeRl.', 2, 'placeholder.jpg', NULL),
-(6, 'Imane', 'Poki', '1992-02-05', 'F', 'Pokimane', '$2y$10$Je1SscAi6L7GSczvSQYefeibqFTds1sikN/hBiK/.AVN5dNMURiFy', 1, '043906d634f43bee460d079c0e168e2efcdca68507e00277dac2fbe178557825.jpeg', '2018-03-03 17:20:18'),
+(6, 'Imane', 'Poki', '1992-02-05', 'F', 'Pokimane', '$2y$10$Je1SscAi6L7GSczvSQYefeibqFTds1sikN/hBiK/.AVN5dNMURiFy', 1, '043906d634f43bee460d079c0e168e2efcdca68507e00277dac2fbe178557825.jpeg', '2018-03-12 14:44:48'),
 (7, 'Scar', 'Ra', NULL, NULL, 'Scarra', '$2y$10$hW.koNHM1BI2KoS.8Oe8luAFWJjFUE21pWV1BjCK8rplzKt8aAtV6', 1, '5ec7ca4cfeb70effc27c913ee7c26c84e7bcce0bac1155eb8b71a748dfd4cfdb.jpeg', '2018-03-03 14:15:19'),
 (8, 'Chan', 'Rebecca', NULL, NULL, 'Pecca', '$2y$10$f9uTgwtXAClZIhCz8slGEuMrbIJ1mky.vaL5KKcPbBn2s1RQDdAa6', 1, '47187873db9b949200519a48bd969cc8271f872b363bb563adca3663aec34fc6.jpeg', NULL),
-(9, 'Chan', 'Chris', NULL, NULL, 'ChrisTo', '$2y$10$Gb0XsJ5nxvbA8Tr5D/cLMOIpe.JNrwVLrZfGIpOhcWywGDCwJFl.K', 1, '13704f8eba8a9388ce61d013fd54e60c04d4b8a87ac012d8b6bc0959838fd137.jpeg', NULL);
+(9, 'Chan', 'Chris', NULL, NULL, 'ChrisTo', '$2y$10$Gb0XsJ5nxvbA8Tr5D/cLMOIpe.JNrwVLrZfGIpOhcWywGDCwJFl.K', 1, '13704f8eba8a9388ce61d013fd54e60c04d4b8a87ac012d8b6bc0959838fd137.jpeg', NULL),
+(10, 'legendre', 'lea', NULL, NULL, 'lege0007', '$2y$10$ahz3RNHtUrp0q/JfVO/1QurKjRZKtHhKu1aabDV7kbhTrKi/2aJZu', 1, 'placeholder.jpg', '2018-03-12 14:44:53');
 
 --
--- Index pour les tables déchargées
+-- Index pour les tables exportées
 --
 
 --
@@ -185,35 +182,31 @@ ALTER TABLE `utilisateurs`
   ADD KEY `FK_Utilisateurs_colocation_id` (`colocation_id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
 -- AUTO_INCREMENT pour la table `colocations`
 --
 ALTER TABLE `colocations`
-  MODIFY `colocation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `colocation_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `factures`
 --
 ALTER TABLE `factures`
   MODIFY `facture_id` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT pour la table `paiements`
 --
 ALTER TABLE `paiements`
-  MODIFY `paiement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+  MODIFY `paiement_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `utilisateur_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+  MODIFY `utilisateur_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
--- Contraintes pour les tables déchargées
+-- Contraintes pour les tables exportées
 --
 
 --
@@ -246,7 +239,6 @@ ALTER TABLE `participer`
 --
 ALTER TABLE `utilisateurs`
   ADD CONSTRAINT `FK_Utilisateurs_colocation_id` FOREIGN KEY (`colocation_id`) REFERENCES `colocations` (`colocation_id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
